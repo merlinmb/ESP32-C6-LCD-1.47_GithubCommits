a30dad0db54565f7f8cbcd76a3e9e8b0aca87bf9
merlinmb <markbeets@gmail.com>
2026-04-08 21:50:16 +0100

    feat: add RGB LED config fields to Config struct

--- src/config.h
+++ src/config.h
@@ -11,6 +11,9 @@ struct Config {
     uint16_t refresh_interval_min;
     uint8_t  anim_top_pct;
     uint16_t anim_period_ms;
+    uint16_t rgb_period_min_ms;  // fastest breath period at max streak (default 1200)
+    uint16_t rgb_period_max_ms;  // slowest breath period at streak 0 (default 8000)
+    uint8_t  rgb_streak_max;     // streak length that achieves min period (default 30)
 };
 
 void config_load(Config &cfg);
